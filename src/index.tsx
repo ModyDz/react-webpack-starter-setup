39d4140c5df4257361e93378de3173328d220ec1
ModyDz <79874169+ModyDz@users.noreply.github.com>
2022-04-16 07:56:06 +0100

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const container = document.getElementById("root");
+
+//@ts-ignore
+const root = ReactDOM.createRoot(container);
+
+// Initial render
+root.render(<App />);
